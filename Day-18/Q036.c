a92530dd0203a50f443f26d80cf91866e18cc9af
saurish36 <saurishrai0306@gmail.com>
2026-09-18 13:08:32 +0530

    Q-36

---
+++ Day-18/Q036.c
@@ -0,0 +1,16 @@
+/*
+Question 36: Print a right triangle star pattern using nested loops.
+*/
+
+#include <stdio.h>
+int main()
+{
+    int row, col;
+    for (row=1; row<=5; row++)
+    {
+        for (col=1; col<=row; col++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
