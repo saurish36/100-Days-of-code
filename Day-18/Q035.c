f36bc4906a6bedfc9bd947ecfc26e12dfd85bbf6
saurish36 <saurishrai0306@gmail.com>
2026-09-18 13:08:02 +0530

    Q-35
    This program prints a square star pattern of size 5x5 using nested loops.

---
+++ Day-18/Q035.c
@@ -0,0 +1,16 @@
+/*
+Question 35: Print a square star pattern using nested loops.
+*/
+
+#include <stdio.h>
+int main()
+{
+    int row, col;
+    for (row=1; row<=5; row++)
+    {
+        for (col=1; col<=5; col++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
