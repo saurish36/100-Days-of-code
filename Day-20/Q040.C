6a6c9c14849bf333fca268b87eae7085b1744953
saurish36 <saurishrai0306@gmail.com>
2026-09-18 13:12:24 +0530

    Q-40

---
+++ Day-20/Q040.C
@@ -0,0 +1,15 @@
+/*
+Question 40: Read five integers into an array and print them.
+*/
+
+#include <stdio.h>
+int main()
+{
+    int a[5], i;
+    printf("Enter 5 integers:\n");
+    for (i=0; i<5; i++) scanf("%d", &a[i]);
+    printf("Array elements are: ");
+    for (i=0; i<5; i++) printf("%d ", a[i]);
+    printf("\n");
+    return 0;
+}
