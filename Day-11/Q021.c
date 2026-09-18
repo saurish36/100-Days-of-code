680119ee67063bac82c88900ed3eb97f95ba03c4
saurish36 <saurishrai0306@gmail.com>
2026-09-18 12:47:42 +0530

    Q-20

---
+++ Day-11/Q021.c
@@ -0,0 +1,18 @@
+/*
+Question 21: Print numbers from 1 to n using a while loop.
+*/
+
+#include <stdio.h>
+int main()
+{
+    int n, i=1;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    while (i <= n)
+    {
+        printf("%d ", i);
+        i++;
+    }
+    printf("\n");
+    return 0;
+}
