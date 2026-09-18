c1e879b6cccccca7f03e27ec09be8fd49f2e1ee0
saurish36 <saurishrai0306@gmail.com>
2026-09-18 13:09:33 +0530

    Q-37

---
+++ Day-19/Q037.c
@@ -0,0 +1,16 @@
+/*
+Question 37: Print a number triangle pattern using nested loops.
+*/
+
+#include <stdio.h>
+int main()
+{
+    int row, col;
+    for (row=1; row<=5; row++)
+    {
+        for (col=1; col<=row; col++)
+            printf("%d", col);
+        printf("\n");
+    }
+    return 0;
+}
