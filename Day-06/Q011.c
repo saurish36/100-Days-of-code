9b81e5dab104baafc4e6d8d7108e43d5b432446b
saurish36 <saurishrai0306@gmail.com>
2026-09-18 12:33:05 +0530

    Q-11

---
+++ Day-06/Q011.c
@@ -0,0 +1,18 @@
+/*
+Question 11: Find the largest of two numbers using if-else.
+*/
+
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    if (a > b)
+        printf("%d is larger.\n", a);
+    else if (b > a)
+        printf("%d is larger.\n", b);
+    else
+        printf("Both numbers are equal.\n");
+    return 0;
+}
